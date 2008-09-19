b273090e23c1484db30a6e3cc198c3f3ce87ea85
Maurits van Rees <maurits@vanrees.org>
2008-09-19 12:24:36 +0000

    Back to development
    svn path=/buildout/collective.recipe.backup/trunk/; revision=72185

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.5'
+version = '0.6dev'
 
 long_description = (
     read('README.txt')
