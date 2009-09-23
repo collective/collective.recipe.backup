b921ecba7cb59913959738cc7709e5cf15d6697a
Reinout van Rees <reinout@vanrees.org>
2009-09-23 07:27:39 +0000

    No need for __file__ in method that opens readme&friends.
    svn path=/buildout/collective.recipe.backup/trunk/; revision=98069

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 
 def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    return open(os.path.join(*rnames)).read()
 
 version = '1.2dev'
 
