ea84e6defcf8d5d9e244683e41d6f3d6e87655f8
Maurits van Rees <maurits@vanrees.org>
2010-08-06 14:31:44 +0000

    Back to development: 1.5
    svn path=/buildout/collective.recipe.backup/trunk/; revision=123041

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(*rnames)).read()
 
-version = '1.4'
+version = '1.5dev'
 
 long_description = (
     read('README.txt')
