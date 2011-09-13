ea6b0ecdaea7a7817205feced9a27bf8486746fd
Maurits van Rees <maurits@vanrees.org>
2011-09-13 23:32:26 +0000

    Preparing release 2.2
    svn path=/buildout/collective.recipe.backup/trunk/; revision=244430

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(*rnames)).read()
 
-version = '2.2dev'
+version = '2.2'
 
 long_description = (
     read('README.txt')
