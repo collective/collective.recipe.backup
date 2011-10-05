493ae28f46a57f2730424cc45a219c4de6c323a0
Maurits van Rees <maurits@vanrees.org>
2011-10-05 16:01:20 +0000

    Back to development: 2.4
    svn path=/buildout/collective.recipe.backup/trunk/; revision=245118

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(*rnames)).read()
 
-version = '2.3'
+version = '2.4dev'
 
 long_description = (
     read('README.txt')
