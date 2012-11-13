d5621a262d62c1330ea5b197e7d73f6dc9c816dc
Maurits van Rees <maurits@vanrees.org>
2012-11-13 09:54:15 +0100

    Back to development: 2.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(*rnames)).read()
 
-version = '2.8'
+version = '2.9.dev0'
 
 long_description = (
     read('README.rst')
