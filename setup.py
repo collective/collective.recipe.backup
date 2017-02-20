6644af2b8dc1c52f56acfc5177d1d6f893120752
Maurits van Rees <maurits@vanrees.org>
2017-02-20 11:01:38 +0100

    setup.py code cleanup

--- setup.py
+++ setup.py
@@ -27,47 +27,49 @@ long_description = (
 entry_point = 'collective.recipe.backup:Recipe'
 entry_points = {
     'zc.buildout': ["default = %s" % entry_point],
-    #'console_scripts':[
-    #        'backup = collective.recipe.backup.repozorunner:main'
-    #        ]
 }
 
-tests_require = ['zope.testing', 'zc.buildout[test]', 'zc.recipe.egg']
+tests_require = [
+    'zope.testing',
+    'zc.buildout[test]',
+    'zc.recipe.egg',
+]
 
-setup(name='collective.recipe.backup',
-      version=version,
-      description="bin/backup script: sensible defaults around bin/repozo",
-      long_description=long_description,
-      classifiers=[
-          'Framework :: Buildout',
-          'Intended Audience :: Developers',
-          'Intended Audience :: System Administrators',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.4',
-          'Programming Language :: Python :: 2.5',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: Software Development :: Build Tools',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          ],
-      keywords='buildout backup repozo zope',
-      author='Reinout van Rees, Maurits van Rees',
-      author_email='reinout@vanrees.org',
-      url='https://github.com/collective/collective.recipe.backup',
-      license='GPL',
-      package_dir={'': 'src'},
-      packages=find_packages('src'),
-      namespace_packages=['collective', 'collective.recipe'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['zc.buildout',
-                        'setuptools',
-                        # -*- Extra requirements: -*-
-                        'zc.recipe.egg',
-                        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='collective.recipe.backup.tests.test_docs.test_suite',
-      entry_points=entry_points,
-      )
+setup(
+    name='collective.recipe.backup',
+    version=version,
+    description="bin/backup script: sensible defaults around bin/repozo",
+    long_description=long_description,
+    classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+    ],
+    keywords='buildout backup repozo zope',
+    author='Reinout van Rees, Maurits van Rees',
+    author_email='reinout@vanrees.org',
+    url='https://github.com/collective/collective.recipe.backup',
+    license='GPL',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    namespace_packages=['collective', 'collective.recipe'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'zc.buildout',
+        'setuptools',
+        'zc.recipe.egg',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    test_suite='collective.recipe.backup.tests.test_docs.test_suite',
+    entry_points=entry_points,
+)
