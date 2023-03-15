cff973494cbd3ff002c3ac7900fe9e2271f11b7d
Maurits van Rees <maurits@vanrees.org>
2023-03-15 15:20:06 +0100

    Add python_requires 3.8+.
    [ci skip]

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
     namespace_packages=["collective", "collective.recipe"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     install_requires=[
         "zc.buildout",
         "setuptools",
