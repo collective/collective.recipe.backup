5318fbd8e647b49997e0e3ffa307f7d952e8baff
Maurits van Rees <maurits@vanrees.org>
2025-04-07 13:32:54 +0200

    Bumped version for feature release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*rnames):
     return open(os.path.join(*rnames)).read()
 
 
-version = "5.0.1.dev0"
+version = "5.1.0.dev0"
 
 long_description = (
     read("README.rst") + "\n" + "Contributors\n"
