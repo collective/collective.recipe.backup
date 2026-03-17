f1810fe621306385ce82a84f51ea84e9c32272bb
Maurits van Rees <maurits@vanrees.org>
2026-03-17 10:49:48 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*rnames):
     return open(os.path.join(*rnames)).read()
 
 
-version = "5.1.1.dev0"
+version = "6.0.0.dev0"
 
 long_description = (
     read("README.rst") + "\n" + "Contributors\n"
