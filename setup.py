2016f985f946c0cf9f8016002092104afc863230
Maurits van Rees <maurits@vanrees.org>
2026-04-10 14:38:23 +0200

    Manually cleanup setup.py.

--- setup.py
+++ setup.py
@@ -4,22 +4,5 @@ This module contains the tool of collective.recipe.backup
 
 from setuptools import setup
 
-import os
-
-
-def read(*rnames):
-    return open(os.path.join(*rnames)).read()
-
-
-version = "6.0.0.dev0"
-
-long_description = (
-    read("README.rst") + "\n" + "Contributors\n"
-    "************\n" + "\n" + read("CONTRIBUTORS.rst") + "\n" + "Change history\n"
-    "**************\n" + "\n" + read("CHANGES.rst")
-)
-
 # See pyproject.toml for package metadata
-setup(
-    long_description=long_description,
-)
+setup()
